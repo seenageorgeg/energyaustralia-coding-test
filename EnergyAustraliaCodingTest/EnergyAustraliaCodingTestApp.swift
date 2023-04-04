89c35d0022db826ef4cdd0f5f1edcbc2b8a0e85f
Seena <seena.georgeg05@gmail.com>
2023-04-04 23:05:37 +0530

    Initial Commit

---
+++ EnergyAustraliaCodingTest/EnergyAustraliaCodingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnergyAustraliaCodingTestApp.swift
+//  EnergyAustraliaCodingTest
+//
+//  Created by John Geevarghese on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct EnergyAustraliaCodingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
